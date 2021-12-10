b8caaad442ae82c6506727c0f901044bff4be93f
Marco di Vincenzo <marco@noah.energy>
2021-12-10 11:26:50 +0100

    fix typo

--- eu-west-1/prod/s3/main.tf
+++ eu-west-1/prod/s3/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "test-bucket-1" {
 
   tags = {
     Environment = "${var.environment}"
-    region      = ${var.aws_region}
+    region      = "${var.aws_region}"
   }
 }
 resource "aws_s3_bucket" "test-bucket-2" {
