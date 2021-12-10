e7e14a3a7198eaeee170ce188d500e44b4e36007
Marco di Vincenzo <marco@noah.energy>
2021-12-10 14:56:57 +0100

    fixing issue with tfsec

--- eu-west-1/test/s3/main.tf
+++ eu-west-1/test/s3/main.tf
@@ -1,15 +1,38 @@
 resource "aws_s3_bucket" "test-bucket-1" {
   bucket = "test-bucket-1-${var.environment}-${var.aws_region}"
   acl    = "private"
+  block_public_acls   = true
+  block_public_policy = true
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  }
 
   tags = {
     Environment = "${var.environment}"
     region      = "${var.aws_region}"
   }
 }
+
 resource "aws_s3_bucket" "test-bucket-2" {
   bucket = "test-bucket-2-${var.environment}-${var.aws_region}"
   acl    = "private"
+  block_public_acls   = true
+  block_public_policy = true
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  }  
 
   tags = {
     Environment = "${var.environment}"
@@ -20,6 +43,17 @@ resource "aws_s3_bucket" "test-bucket-2" {
 resource "aws_s3_bucket" "test-bucket-3" {
   bucket = "test-bucket-3-${var.environment}-${var.aws_region}"
   acl    = "private"
+  block_public_acls   = true
+  block_public_policy = true
+  
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  }
 
   tags = {
     Environment = "${var.environment}"
