47cda5297de31570e2451ec0842f79c3617ef82d
Marco di Vincenzo <marco@noah.energy>
2021-12-10 11:24:59 +0100

    testing security lint tf project

--- eu-west-1/prod/s3/main.tf
+++ eu-west-1/prod/s3/main.tf
@@ -1,15 +1,15 @@
 resource "aws_s3_bucket" "test-bucket-1" {
   bucket = "test-bucket-1-${var.environment}-${var.aws_region}"
-  acl    = "private"
+  acl    = "public"
 
   tags = {
     Environment = "${var.environment}"
-    region      = "${var.aws_region}"
+    region      = ${var.aws_region}
   }
 }
 resource "aws_s3_bucket" "test-bucket-2" {
   bucket = "test-bucket-2-${var.environment}-${var.aws_region}"
-  acl    = "private"
+  acl    = "public"
 
   tags = {
     Environment = "${var.environment}"
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "test-bucket-2" {
 
 resource "aws_s3_bucket" "test-bucket-3" {
   bucket = "test-bucket-3-${var.environment}-${var.aws_region}"
-  acl    = "private"
+  acl    = "public"
 
   tags = {
     Environment = "${var.environment}"
