9d05edccf349a22313f3eb2e33a71add98fbd649
Marco di Vincenzo <marco@noah.energy>
2021-12-13 16:31:47 +0100

    test provider

--- test-s3/s3/main.tf
+++ test-s3/s3/main.tf
@@ -5,9 +5,7 @@
 } */
 
 provider "aws" {
-  region                  = "${var.region}"
-  access_key              = "${AWS_ACCESS_KEY_ID}"
-  secret_key              = "${AWS_SECRET_ACCESS_KEY}"
+  region = "eu-west-1"
 }
 
 resource "aws_s3_bucket" "bucket" {
