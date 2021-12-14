4a3c4c3e0876fd5130547a7cecc07c53980c251b
Marco di Vincenzo <marco@noah.energy>
2021-12-14 11:08:21 +0100

    fixed key backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tfstate-infra-test"
-    key            = "terraform_state"
+    key            = "terraform_state/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "stateLockingTf"
   }
