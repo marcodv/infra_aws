f164d436d204e201be400582cb526592bbe0f606
Marco di Vincenzo <marco@noah.energy>
2022-02-07 20:40:50 +0100

    add message to config

--- modules/eks/provider.tf
+++ modules/eks/provider.tf
@@ -30,5 +30,7 @@ provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
+  // the following config fix this issue
+  //https://github.com/gavinbunney/terraform-provider-kubectl/issues/35
   load_config_file       = false
 }
