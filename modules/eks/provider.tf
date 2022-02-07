1cc0a4afa25b11b57f49b243167926aedfa3acd4
Marco di Vincenzo <marco@noah.energy>
2022-02-07 14:58:26 +0100

    added metrics server to eks setup

--- modules/eks/provider.tf
+++ modules/eks/provider.tf
@@ -11,3 +11,18 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
+
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+  }
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
