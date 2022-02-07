d6aa8603edfccab472f917471edb44f75bf71d1e
Marco di Vincenzo <marco@noah.energy>
2022-02-07 20:25:00 +0100

    test load_config_file option

--- modules/eks/metrics_server.tf
+++ modules/eks/metrics_server.tf
@@ -1,8 +1,9 @@
+// Apply the server manifest during eks fails.
+// Need to find a way to pass it after deploy EKS
 data "kubectl_file_documents" "metrics_server" {
   content = file("${path.module}/manifests/metrics_server/metrics_server.yaml")
-}
+} 
 
-// Apply the manifest passed from kubectl_file_documents
 resource "kubectl_manifest" "setup_metrics_server" {
   depends_on = [aws_eks_cluster.eks_cluster]
   for_each   = data.kubectl_file_documents.metrics_server.manifests
