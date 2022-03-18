fa37b70adfe22ae16bf6e3c8b88e616a4ba3df83
Marco di Vincenzo <marco@noah.energy>
2022-03-18 12:52:37 +0100

    removed dev env and only prod

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -31,6 +31,7 @@ provider "aws" {
   }
 }
 
+/*
 module "networking" {
   source = "../../modules/vpc/"
 
@@ -75,3 +76,4 @@ module "jump_host" {
   public_subnets_id  = module.networking.public_subnets_id
   bastions_sg        = [module.networking.bastions_sg, module.networking.eks_sg]
 }
+*/
--- modules/bastions/kubectl_repo.sh
+++ modules/bastions/kubectl_repo.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-cat <<YUM_REPO | sudo tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-YUM_REPO
-
 sudo yum install -y kubectl gcc
 wget http://download.redis.io/redis-stable.tar.gz && tar xvzf redis-stable.tar.gz && cd redis-stable && make
 sudo cp src/redis-cli /usr/bin/
