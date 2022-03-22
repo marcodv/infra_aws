2249d761c7ac0f0878f55b1269425a49bbf028fb
Marco di Vincenzo <marco@noah.energy>
2022-03-22 13:56:30 +0100

    set old ignore case

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -25,6 +25,7 @@ data "aws_caller_identity" "current" {}
 
 # Create EKS cluster 
 #tfsec:ignore:aws-eks-encrypt-secrets
+#tfsec-ignore:aws-eks-no-public-cluster-access-to-cidr 
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "eks-${var.environment}-env"
   role_arn = "arn:aws:iam::848481299679:role/eks-role-${var.environment}-env"
@@ -33,7 +34,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   vpc_config {
     security_group_ids     = [var.eks_sg]
     subnet_ids             = var.eks_subnets
-    #tfsec-ignore:aws-eks-no-public-cluster-access-to-cidr 
+    #tfsec:ignore:aws-eks-no-public-cluster-access 
     endpoint_public_access = true
     //endpoint_private_access = true
   }
