c5d1d0feeef706f139e54f5fc01a702890af204d
Marco di Vincenzo <marco@noah.energy>
2022-04-06 11:34:48 +0200

    disable auth logs for eks

--- modules/eks/provider.tf
+++ modules/eks/provider.tf
@@ -2,9 +2,17 @@ terraform {
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.10.0"
+      version = "~> 1.13.1"
+    }/*
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.8.0"
+    }    
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.6.0"
     }
-  }
+  }*/
 }
 
 data "aws_eks_cluster" "cluster" {
