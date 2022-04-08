6d2dd65eff82a9096f7ceec8e724c57885e67cbe
Marco di Vincenzo <marco@noah.energy>
2022-04-08 09:45:20 +0200

    reverted initial changes

--- modules/eks/provider.tf
+++ modules/eks/provider.tf
@@ -24,11 +24,7 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", "eks-${var.environment}-env"]
-  }
+  token                  = data.aws_eks_cluster_auth.cluster.token
   host                   = aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
